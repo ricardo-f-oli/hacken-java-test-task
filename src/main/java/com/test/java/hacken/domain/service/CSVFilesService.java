04fffa6627a3a3f9ebfc53a2b9675d4d881376af
Ricardo Oliveira <ricardo.fer.oliveira964@gmail.com>
2024-06-29 12:39:47 -0300

    Creating controller and domain for the application

---
+++ src/main/java/com/test/java/hacken/domain/service/CSVFilesService.java
@@ -0,0 +1,14 @@
+package com.test.java.hacken.domain.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class CSVFilesService {
+    public CSVFilesService(){}
+
+//    public void uploadCSVFiles(CSVFile csvFile){
+//
+//    }
+}
