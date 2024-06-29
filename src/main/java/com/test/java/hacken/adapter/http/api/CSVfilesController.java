55c3785ed7a00cf0c905eb6c7132a66eb76de846
Ricardo Oliveira <ricardo.fer.oliveira964@gmail.com>
2024-06-29 18:30:23 -0300

    Fixing upload method and adding OpenCSV to pom

--- src/main/java/com/test/java/hacken/adapter/http/api/CSVfilesController.java
+++ src/main/java/com/test/java/hacken/adapter/http/api/CSVfilesController.java
@@ -1,18 +1,23 @@
 package com.test.java.hacken.adapter.http.api;
 
+import com.test.java.hacken.domain.service.CSVFilesService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/v1")
 public class CSVfilesController {
-    public CSVfilesController(){}
-
-    @PostMapping("/csv")
-    public void uploadCSVfiles(){}
+    private final CSVFilesService csvFilesService;
+    public CSVfilesController(CSVFilesService csvFilesService){
+        this.csvFilesService = csvFilesService;
+    }
+    @PostMapping(value = "/csv", consumes = {"multipart/form-data"})
+    public ResponseEntity<Integer> uploadCSVfiles(
+            @RequestPart("file")MultipartFile file
+            ){
+        return ResponseEntity.ok(csvFilesService.uploadCSVFiles(file));
+    }
 
 //    @GetMapping
 //    public ResponseEntity<> searchCSVfile(){}
--- src/main/java/com/test/java/hacken/domain/service/CSVFilesService.java
+++ src/main/java/com/test/java/hacken/domain/service/CSVFilesService.java
@@ -2,13 +2,14 @@ package com.test.java.hacken.domain.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Service
 public class CSVFilesService {
     public CSVFilesService(){}
 
-//    public void uploadCSVFiles(CSVFile csvFile){
-//
-//    }
+    public Integer uploadCSVFiles(MultipartFile csvFile){
+
+    }
 }
