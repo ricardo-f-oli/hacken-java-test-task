ebdacc802f539820ceef730f78d530df7e457296
Ricardo Oliveira <ricardo.fer.oliveira964@gmail.com>
2024-07-01 12:34:46 -0300

    Adding prometheus and metrics configs to project

--- src/main/java/com/test/java/hacken/domain/service/CSVFilesSearchService.java
+++ src/main/java/com/test/java/hacken/domain/service/CSVFilesSearchService.java
@@ -4,6 +4,8 @@ import com.opencsv.CSVReader;
 import com.test.java.hacken.domain.entites.GenericCSVEntity;
 import com.test.java.hacken.domain.entites.GenericCSVFileEntity;
 import com.test.java.hacken.domain.repository.GenericCSVFileRepository;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,17 @@ import java.util.List;
 
 @Service
 public class CSVFilesSearchService {
-    @Autowired
-    private GenericCSVFileRepository genericCSVFileRepository;
 
+    private final GenericCSVFileRepository genericCSVFileRepository;
+    private final Counter searchCounter;
+
+    @Autowired
+    public CSVFilesSearchService(GenericCSVFileRepository genericCSVFileRepository, MeterRegistry meterRegistry) {
+        this.genericCSVFileRepository = genericCSVFileRepository;
+        this.searchCounter = meterRegistry.counter("csv.searches");
+    }
     public List<GenericCSVEntity> search(String query) {
+        searchCounter.increment();
         List<GenericCSVEntity> results = new ArrayList<>();
         List<GenericCSVFileEntity> csvFiles = genericCSVFileRepository.findAll();
 
--- src/main/java/com/test/java/hacken/domain/service/CSVFilesUploadService.java
+++ src/main/java/com/test/java/hacken/domain/service/CSVFilesUploadService.java
@@ -3,6 +3,8 @@ package com.test.java.hacken.domain.service;
 import com.opencsv.CSVReader;
 import com.test.java.hacken.domain.entites.GenericCSVFileEntity;
 import com.test.java.hacken.domain.repository.GenericCSVFileRepository;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,10 +21,18 @@ import java.util.Map;
 
 @Service
 public class CSVFilesUploadService {
-    @Autowired
-    private GenericCSVFileRepository genericCSVFileRepository;
 
+    private final GenericCSVFileRepository genericCSVFileRepository;
+
+    private final Counter uploadCounter;
+
+    @Autowired
+    public CSVFilesUploadService(GenericCSVFileRepository genericCSVFileRepository, MeterRegistry meterRegistry) {
+        this.genericCSVFileRepository = genericCSVFileRepository;
+        this.uploadCounter = meterRegistry.counter("csv.uploads");
+    }
     public Long uploadCSVFiles(MultipartFile file)  throws IOException {
+        uploadCounter.increment();
         String content = new String(file.getBytes());
         GenericCSVFileEntity csvFile = new GenericCSVFileEntity();
         csvFile.setName(file.getOriginalFilename());
